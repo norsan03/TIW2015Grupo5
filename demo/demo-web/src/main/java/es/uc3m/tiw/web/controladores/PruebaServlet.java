2822c0fa49677a8dd60e065cbce428336896470c
dpalomar <dpalomar@inf.uc3m.es>
2015-09-24 18:38:26 +0200

    Ejemplo PruebaServlet y paso de parametros

---
+++ demo/demo-web/src/main/java/es/uc3m/tiw/web/controladores/PruebaServlet.java
@@ -0,0 +1,52 @@
+package es.uc3m.tiw.web.controladores;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PruebaServlet
+ * @author David Palomar
+ */
+@WebServlet(value="/saludos",loadOnStartup=1)
+public class PruebaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PruebaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Peticion recibida por GET");
+		out.println("<h1>Saludos "+nombre +"</h1>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Peticion recibida por POST");
+		out.println("<h1>Saludos "+nombre+"</h1>");
+		out.close();
+	}
+
+}
